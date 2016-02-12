71fb09839266d7f2356877cca3482c45fb7c5632
Vikram Mahendru <vikram.r.mahendru@gmail.com>
2016-02-12 16:44:08 -0500

    Initial Commit

---
+++ GameV.01Tests/GameV_01Tests.swift
@@ -0,0 +1,36 @@
+//
+//  GameV_01Tests.swift
+//  GameV.01Tests
+//
+//  Created by Vikram Mahendru on 2/12/16.
+//  Copyright © 2016 Vikram Mahendru. All rights reserved.
+//
+
+import XCTest
+@testable import GameV_01
+
+class GameV_01Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
